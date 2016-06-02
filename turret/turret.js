99392cee25f4140068f8e342049c78523cf1a234
paolorechia <paolorechia@gmail.com>
2016-06-02 01:42:17 -0300

    background gerado salvo - pequena (inutil) funcao para redesenhar o background

--- turret/turret.js
+++ turret/turret.js
@@ -12,7 +12,10 @@ for (i = 0; i < 80; i++){
         array[i] = [];
 }
 
-
+//redesenha o background
+function blitBackground(background){
+    ctx.putImageData(background, 0, 0);
+}
 
 // preenche background com pontos brancos que representam estrelas
 function populaEstrelas(ctx, num){
@@ -28,6 +31,8 @@ function populaEstrelas(ctx, num){
 // desenha o turret pela primeira vez (Apenas!)
 // nas próximas vezes imagino que deve ser usado uma rotação
 
+
+// funcao desenha esta em desuso, mantida no codigo soh para reverter se algo der errado
 function desenhaCanhao(ctx, raio, angulo){
     ctx.strokeStyle = "#f0470e";
     ctx.moveTo(width/2, height/2);
@@ -35,20 +40,12 @@ function desenhaCanhao(ctx, raio, angulo){
     ctx.lineTo(width/2, height/2 - raio * 2);
     ctx.stroke();
 }
-function desenhaTurret(ctx, raio, angulo){
-    ctx.beginPath();
-    ctx.arc(width/2, height/2, raio, 0, 2*Math.PI);
-    ctx.strokeStyle = "#f0470e";
-    ctx.stroke();
-    ctx.fillStyle = "#005252";
-    ctx.fill();
-    rotacionaCanhao(ctx, raio, angulo);
-}
-
 
 // a funcao "rotate" rotaciona o Canvas INTEIRO
 // dah para implementar coisas legais com isso
-// tentativa de rotacionar o canhao
+
+// rotacionaCanhao na realidade desenha
+// o canhao no angulo dado
 
 function rotacionaCanhao(ctx, raio, angulo){
     ctx.save();
@@ -60,6 +57,16 @@ function rotacionaCanhao(ctx, raio, angulo){
     ctx.stroke();
     ctx.restore();
 }
+function desenhaTurret(ctx, raio, angulo){
+    ctx.beginPath();
+    ctx.arc(width/2, height/2, raio, 0, 2*Math.PI);
+    ctx.strokeStyle = "#f0470e";
+    ctx.stroke();
+    ctx.fillStyle = "#005252";
+    ctx.fill();
+    rotacionaCanhao(ctx, raio, angulo);
+}
+
 
 // testando firebug
 console.log("1 c4n 7yp3 t0 c0ns0l3!");
@@ -74,9 +81,13 @@ ctx.fillRect(0,0,800,600);
 // e com estrelas
 populaEstrelas(ctx, 200);
 
+// salva background criado
+var background = ctx.getImageData(0,0,800,600)
+
 // desenha turret
 var raio = 15;
 desenhaTurret(ctx, raio);
 for (i=0; i< 10; i++)
+    blitBackground(background);   // blita background
     desenhaTurret(ctx, raio, i * 10);
 
