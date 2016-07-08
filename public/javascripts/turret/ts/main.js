b3c2fc230b8b3101839e6d95be55e8b9029eba40
Diogo <diogo_miloco@hotmail.com>
2016-07-08 17:02:32 -0300

    Remoção do TS e organização das pastas (sem o TS)

--- public/javascripts/turret/ts/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-console.log("i'm the main");
-var asteroide_ts_1 = require('./asteroide.ts');
-var asteroide1 = new asteroide_ts_1.Asteroide(3, 10, 10, 2, [3, 2]);
-console.log(asteroide1.show_position());
