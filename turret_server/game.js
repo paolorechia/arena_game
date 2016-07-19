102e08489fe93e3ca17a3be51d72b56ff3dce10f
Paolo Andreas Stall Rechia <pasr13@inf.ufpr.br>
2016-07-19 18:32:37 -0300

    funcoes que usam o socket deslocadas para network.js

--- turret_server/game.js
+++ turret_server/game.js
@@ -1,50 +1,15 @@
 // main loop of server-side game
 module.exports = function(io){
     var module = {};
-    var background = {
-        height : 0,
-        width : 0,
-    }
+
     var height=3000;
     var width=5000;
 
     var calc = require('./calculo.js');
     var turret = require ('./turret.js')(height,width, calc);
     var ast = require('./asteroides.js')(height,width, calc);
-
-    io.set('transports', ['websocket']);
     var players = {length : 0};
-    io.on('connection', function(socket){
-        players[socket.id] = turret.cria();
-        players.length++;
-        socket.emit('myid', socket.id);
-        console.log(socket.id + " has connected");
-        socket.on('direcao', function(key){
-            console.log("recebi direcao: " + key);
-            turret.atualizaDirecao(key, socket.id, players);
-            socket.emit('direcao', (key));
-        });
-        socket.on('disconnect', function(){
-            console.log(socket.id + " has disconnected");
-            delete players[socket.id];
-            players.length--;
-            console.log(players);
-
-        });
-        socket.on('myid', function(id){
-            console.log(players.length);
-        });
-        console.log(players);
-    });
-
-    console.log(background);
-    var i = 0;
-    var j = 0;
-    function enviaAsteroides(){
-        io.sockets.emit('asteroides', ast.asteroides.vetor);
-    //    console.log("enviando... " + ast.asteroides.vetor);
-    }
-
+    var net = require('./network.js')(io, players, ast, turret)
     function atualizaTurrets(){
         for (var id in io.sockets.connected){
                 var turret = players[id];
@@ -67,26 +32,8 @@ module.exports = function(io){
     }
 
 
-    // algoritmo horrível, repensar de um jeito mais eficiente
-    function enviaTurrets(){
-        var players_positions = [];
-        var players_id = [];
-        var aux;
-        var j = 0;
-        // monta vetor de posicoes
-        // e manda a posicao do jogador para ele mesmo 
-        for (var id in io.sockets.connected){
-            players_positions[j] = players[id].pos;
-            players_id[j] = id;
-            var socket = io.sockets.connected[id];
-            socket.emit('movimento', players_positions[j]);
-            j++;
-        }
-        // manda a posicao dos outros jogadores
-            io.sockets.emit('players', players_positions);
-    }
-
-
+    var i = 0;
+    // loop principal (infinito)
     module.start = function(){
         i++;
     /*
@@ -112,8 +59,8 @@ module.exports = function(io){
         }
 
         if (i % 20 == 0){
-            enviaTurrets();
-            enviaAsteroides();
+            net.enviaTurrets();
+            net.enviaAsteroides();
         }
 
         setTimeout(module.start, 1);
--- turret_server/network.js
+++ turret_server/network.js
@@ -1,3 +1,56 @@
 // handles the socket communication between server and clients
 
+module.exports = function(io, players, ast, turret){
+    var module = {};
 
+    // setup the HTTP connection to use only websocket and deny polling
+    io.set('transports', ['websocket']);
+
+    // handles the 
+    io.on('connection', function(socket){
+        players[socket.id] = turret.cria();
+        players.length++;
+        socket.emit('myid', socket.id);
+        console.log(socket.id + " has connected");
+        socket.on('direcao', function(key){
+            console.log("recebi direcao: " + key);
+            turret.atualizaDirecao(key, socket.id, players);
+            socket.emit('direcao', (key));
+        });
+        socket.on('disconnect', function(){
+            console.log(socket.id + " has disconnected");
+            delete players[socket.id];
+            players.length--;
+            console.log(players);
+
+        });
+        socket.on('myid', function(id){
+            console.log(players.length);
+        });
+        console.log(players);
+    });
+
+    module.enviaTurrets = function(){
+        var players_positions = [];
+        var players_id = [];
+        var aux;
+        var j = 0;
+        // monta vetor de posicoes
+        // e manda a posicao do jogador para ele mesmo 
+        for (var id in io.sockets.connected){
+            players_positions[j] = players[id].pos;
+            players_id[j] = id;
+            var socket = io.sockets.connected[id];
+            socket.emit('movimento', players_positions[j]);
+            j++;
+        }
+        // manda a posicao dos outros jogadores
+            io.sockets.emit('players', players_positions);
+    }
+    module.enviaAsteroides = function(){
+        io.sockets.emit('asteroides', ast.asteroides.vetor);
+    //    console.log("enviando... " + ast.asteroides.vetor);
+    }
+
+    return module;
+} 
