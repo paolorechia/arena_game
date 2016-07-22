ba4eed56d5df265d89bae9faca50bbc645ec7741
Paolo Andreas Stall Rechia <pasr13@inf.ufpr.br>
2016-07-21 23:25:07 -0300

    adicionando script pra auxiliar na instalação do ec2

---
+++ setup_ec2.sh
@@ -0,0 +1,12 @@
+# bash script para configurar o ec2 automagicamente
+sudo yum -y update
+curl --silent --location https://rpm.nodesource.com/setup_6.x | 
+sudo bash -yum -y install nodejs
+https://bitbucket.org/diogomiloco/arena_game/
+cd arena_game
+npm install express
+npm install jade
+npm install favicon
+npm install cookie-parser
+npm install body-parser
+npm install socket.io
