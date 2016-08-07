83c90d865db23e57da8ed955fc9e13135aefb4b2
Paolo Andreas Stall Rechia <pasr13@inf.ufpr.br>
2016-08-07 07:25:14 -0300

    sons de licenca livre

--- public/javascripts/turret_client/packaged.js
+++ public/javascripts/turret_client/packaged.js
@@ -892,11 +892,32 @@ module.exports = function (camera, background, data){
 module.exports = function(data, ship){
     var module = {};
    
-    var sound = document.getElementById("blaster");
+    var sound_laser = document.getElementById("laser");
+    var sound_blaster = document.getElementById("blaster");
     module.play = function(){
         if (data.atirando == true){    
-            sound.currentTime = 0.07;
-            sound.play();
+            console.log(ship.weapon);
+            if (ship.weapon == 'laser'){
+                console.log(sound_laser.currentTime);
+                if (sound_laser.currentTime == 0){
+                    sound_laser.play();
+                }
+                if (sound_laser.currentTime > 0.15){
+                    sound_laser.currentTime = 0.1;
+                    sound_laser.play();
+                }
+            }
+            else
+                if (ship.weapon == 'blaster'){
+                    if (sound_blaster.currentTime == 0){
+                        sound_blaster.play();
+                    }
+                    if (sound_blaster.currentTime > 0.10){
+                        sound_blaster.currentTime = 0.05;
+                        sound_blaster.play();
+                }
+            }
+                
         }
     }
     return module;
